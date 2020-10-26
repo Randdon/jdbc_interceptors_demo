09694347fcad392c25ea82385911bc7a534a131c
yuandong <yuandong@htkg.cn>
2020-10-26 20:03:27 +0800

    update

--- demo/src/main/java/com/zhouyuan/space/demo/controller/DataController.java
+++ demo/src/main/java/com/zhouyuan/space/demo/controller/DataController.java
@@ -1,15 +1,21 @@
 package com.zhouyuan.space.demo.controller;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.casic.htzy.log.annotation.NetworkLog;
 import com.casic.htzy.log.constant.LogActionEnum;
 import com.zhouyuan.space.demo.entity.Data;
 import com.zhouyuan.space.demo.entity.LogCenter;
 import com.zhouyuan.space.demo.service.DataService;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,7 +30,7 @@ public class DataController {
     @GetMapping(value = "/test")
     @Transactional
     @NetworkLog(description = "http请求测试接口",action = LogActionEnum.READ)
-    public String test(@Param("name") String name, @Param("id") int id) {
+    public String test(@RequestParam("name") String name, @RequestParam("id") int id) {
 
         //List<Data> data = dataService.listAll();
 
@@ -52,7 +58,7 @@ public class DataController {
 
     @PostMapping(value = "/post")
     @Transactional
-    public String test(@Param("name") String name, @Param("id") int id, @RequestBody Data data) {
+    public String test(@RequestParam("name") String name, @RequestParam("id") int id, @RequestBody Data data) {
         dataService.save(data);
         return "ok";
     }
@@ -68,4 +74,87 @@ public class DataController {
         System.out.println(aBoolean == null);
         return logCenterProperties.getSqlType().toString();
     }
+
+    @PostMapping(value = "/upload")
+    @NetworkLog(action = LogActionEnum.CREATE,description = "upload test")
+    public String upload(@RequestParam("name") String name, @RequestParam("id") int id, @RequestParam("file")MultipartFile file) {
+        String fileName = file.getName();
+        String originalFilename = file.getOriginalFilename();
+        String contentType = file.getContentType();
+        long size = file.getSize();
+        JSONObject object = new JSONObject();
+        object.put("fileName",fileName);
+        object.put("originalFilename",originalFilename);
+        object.put("contenType",contentType);
+        object.put("size",size);
+
+        return object.toJSONString();
+    }
+
+    @PostMapping(value = "/uploadMulti")
+    @NetworkLog(action = LogActionEnum.CREATE,description = "upload test")
+    public String uploadMulti(@RequestParam("name") String name, @RequestParam("id") int id, @RequestParam("files")MultipartFile[] files) {
+        JSONArray objects = new JSONArray();
+        for (MultipartFile file : files) {
+
+            String fileName = file.getName();
+            String originalFilename = file.getOriginalFilename();
+            String contentType = file.getContentType();
+            long size = file.getSize();
+            JSONObject object = new JSONObject();
+            object.put("fileName",fileName);
+            object.put("originalFilename",originalFilename);
+            object.put("contenType",contentType);
+            object.put("size",size);
+            objects.add(object);
+        }
+
+        return objects.toJSONString();
+    }
+
+    @GetMapping(value = "/download")
+    @NetworkLog(action = LogActionEnum.READ,description = "download test")
+    public byte[] download(@RequestParam("path") String path) throws IOException {
+        File file = new File(path);
+        byte[] bytes = FileCopyUtils.copyToByteArray(file);
+        return bytes;
+    }
+
+    @GetMapping(value = "/downAttachment")
+    @NetworkLog(action = LogActionEnum.READ,description = "downAttachment test")
+    public void downAttachment(@RequestParam("path") String path, @RequestParam("name") String name, HttpServletResponse response) {
+        try {
+            File file = new File(path);
+            InputStream fis = new BufferedInputStream(new FileInputStream(file));
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            response.setHeader("Content-Disposition", "attachment;filename=" + new String(name.getBytes("utf-8"), "ISO8859-1"));
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            OutputStream toClient = new BufferedOutputStream(response.getOutputStream());
+            String contentType = null;
+            try {
+                // contentType= Files.probeContentType(path);
+                contentType = URLConnection.guessContentTypeFromStream(fis);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            response.setContentType(contentType);
+            if (contentType == null) {
+                response.setContentType("application/x-msdownload");
+            }
+            try {
+                toClient.write(buffer);
+                toClient.flush();
+                toClient.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        response.setContentType("text/html;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+    }
+
 }
